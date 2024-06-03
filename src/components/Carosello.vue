2f37c236dbbb4dd130af4a7175d72e7cbb62ed45
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-03 17:22:21 +0200

    Prima parte del carosello completata

--- src/components/Carosello.vue
+++ src/components/Carosello.vue
@@ -2,8 +2,8 @@
 import store from '../data/store.js'
 export default {
   name: "Carosello",
-  data(){
-    return{
+  data() {
+    return {
       store,
     }
   }
@@ -11,9 +11,27 @@ export default {
 </script>
 
 <template>
-  <div id="caroselloHeight">
-    <div class="w-75">
-      <p>{{ store.CaroselloSottotitolo }}</p>
+  <!-- <div id="caroselloHeight" class="position-relative"
+    v-bind:style="{ 'background-image': `url(${store.CaroselloImages.firstImage})` }"> -->
+    <div id="caroselloHeight" class="position-relative">
+    <img id="backgroundImage":src="store.CaroselloImages.firstImage" alt="">
+
+    <div id="bottoni" class="d-flex justify-content-between align-items-center px-5 h-100">
+      <i class="fa-solid fa-chevron-left previous cursorPointer"></i>
+      <i class="fa-solid fa-chevron-right next cursorPointer"></i>
+    </div>
+
+    <div class="w-75 h-100 d-flex m-auto align-items-center">
+      <div class="w-25">
+        <h1>{{ store.CaroselloTitolo }}</h1>
+        <p>{{ store.CaroselloSottotitolo }}</p>
+        <div class="d-inline-flex align-items-center gap-2 me-5 cursorPointer">
+          <p class="m-0">Learn More</p>
+          <i class="fa-solid fa-arrow-right m"></i>
+        </div>
+      </div>
+
+
     </div>
   </div>
 
@@ -21,7 +39,29 @@ export default {
 
 <style scoped>
 #caroselloHeight {
-  height: 60vh;
-  background-color: red;
+  height: 80vh;
+}
+
+#backgroundImage{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+h1, p, i{
+  color: var(--site-white) !important;
+}
+
+#bottoni {
+  position: absolute;
+  width: 100%;
+  transform: translateY(-50%, -50%);
+  z-index: 1;
+}
+
+.cursorPointer{
+  cursor: pointer;
 }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,10 @@ import { reactive } from 'vue';
 
 const iMieiDati = reactive({
     CaroselloTitolo: 'Buy And Sell Your Car At Its Value',
-    CaroselloSottotitolo: 'Find the right price and dealer.'
+    CaroselloSottotitolo: 'Find the right price and dealer.',
+    CaroselloImages: {
+        firstImage : './src/assets/Img/slider-autocar-5.jpg'
+    }
 
 });
 
