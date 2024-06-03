ce56d108c1f9de7f2f43944b94cfa321b086eed6
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-03 16:24:55 +0200

    Carosello e store sono stati messi in collegamento

--- src/components/Carosello.vue
+++ src/components/Carosello.vue
@@ -1,14 +1,22 @@
 <script>
+import store from '../data/store.js'
 export default {
-  name: "Carosello"
+  name: "Carosello",
+  data(){
+    return{
+      store,
+    }
+  }
 }
 </script>
 
 <template>
-  <div id="caroselloHeight"></div>
-  <div>
-    
+  <div id="caroselloHeight">
+    <div class="w-75">
+      <p>{{ store.CaroselloSottotitolo }}</p>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
---
+++ src/data/store.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue';
+
+const iMieiDati = reactive({
+    CaroselloTitolo: 'Buy And Sell Your Car At Its Value',
+    CaroselloSottotitolo: 'Find the right price and dealer.'
+
+});
+
+export default iMieiDati
