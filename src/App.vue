39000bfbb8c9d90be66a48b389457d1b263c1cc9
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-04 10:24:50 +0200

    Componente SearchCar creato e importato

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import Carosello from './components/Carosello.vue'
+import SearchCar from './components/SearchCar.vue'
 
 export default{
   components:{
-    Carosello
+    Carosello,
+    SearchCar
   }
 }
 </script>
 
 <template>
   <Carosello />
+  <SearchCar />
 </template>
 
 <style scoped>
---
+++ src/components/SearchCar.vue
@@ -0,0 +1,71 @@
+<script>
+import store from '../data/store.js'
+export default {
+  name: "SearchCar",
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+
+  <div id="caroselloHeight" class="position-relative prova">
+      <!-- <div id=" caroselloHeight" class="position-relative">
+    <img id="backgroundImage" :src="store.Carosello[store.number].firstImage" alt=""> -->
+
+  </div>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+#caroselloHeight {
+  height: 90vh;
+}
+
+.prova {
+  background-position: bottom;
+}
+
+#backgroundImage {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+.textWidth {
+  width: 30%;
+}
+
+h1,
+p,
+i {
+  color: var(--site-white) !important;
+}
+
+#bottoni {
+  position: absolute;
+  width: 100%;
+  transform: translateY(-50%, -50%);
+  z-index: 1;
+}
+
+.cursorPointer {
+  cursor: pointer;
+}
+</style>
