0ebeb5d061e135812fe774264b93139c200fdcda
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-04 10:15:00 +0200

    Carosello finito tranne per altezza e script

--- src/components/Carosello.vue
+++ src/components/Carosello.vue
@@ -33,38 +33,49 @@ export default {
 </script>
 
 <template>
-  <div id="caroselloHeight" class="position-relative prova"
-    v-bind:style="{ 'background-image': `url(${store.Carosello[store.number].firstImage})` }">
-    <!-- <div id="caroselloHeight" class="position-relative">
+  <Transition>
+    <div id="caroselloHeight" class="position-relative prova"
+      v-bind:style="{ 'background-image': `url(${store.Carosello[store.number].firstImage})` }">
+      <!-- <div id="caroselloHeight" class="position-relative">
     <img id="backgroundImage" :src="store.Carosello[store.number].firstImage" alt=""> -->
 
-    <div id="bottoni" class="d-flex justify-content-between align-items-center px-4 h-100">
-      <i class="fa-solid fa-chevron-left previous cursorPointer" @click="CaroselloIndietro"></i>
-      <i class="fa-solid fa-chevron-right next cursorPointer" @click="caroselloAvanti"></i>
-    </div>
-
-    <div class="w-75 h-100 position-relative d-flex m-auto align-items-center z-3">
-      <div class="w-25">
-        <h1 class="fw-bold">{{ store.Carosello[0].CaroselloTitolo }}</h1>
-        <p class="fw-bold">{{ store.CaroselloSottotitolo }}</p>
-        <div class="d-inline-flex align-items-center gap-2 me-5 cursorPointer fw-bold">
-          <p class="m-0">Learn More</p>
-          <i class="fa-solid fa-arrow-right"></i>
-        </div>
+      <div id="bottoni" class="d-flex justify-content-between align-items-center px-4 h-100">
+        <i class="fa-solid fa-chevron-left previous cursorPointer" @click="CaroselloIndietro"></i>
+        <i class="fa-solid fa-chevron-right next cursorPointer" @click="caroselloAvanti"></i>
       </div>
 
+      <div class="w-75 h-100 position-relative d-flex m-auto align-items-center z-3">
+        <div class="textWidth">
+          <h1 class="fw-bold">{{ store.Carosello[store.number].CaroselloTitolo }}</h1>
+          <p class="fw-bold">{{ store.CaroselloSottotitolo }}</p>
+          <div class="d-inline-flex align-items-center gap-2 me-5 cursorPointer fw-bold">
+            <p class="m-0">Learn More</p>
+            <i class="fa-solid fa-arrow-right"></i>
+          </div>
+        </div>
 
+
+      </div>
     </div>
-  </div>
-
+  </Transition>
 </template>
 
 <style scoped>
-#caroselloHeight {
-  height: 80vh;
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
 }
 
-.prova{
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+#caroselloHeight {
+  height: 90vh;
+}
+
+.prova {
   background-position: bottom;
 }
 
@@ -74,6 +85,9 @@ export default {
   height: 100%;
   object-fit: cover;
   z-index: -1;
+}
+.textWidth{
+  width: 30%;
 }
 
 h1,
